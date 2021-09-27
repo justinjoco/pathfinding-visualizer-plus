40386544af7b53ac3de5fa99f1e413709a7cef0d
Justin Joco <justin.joco@gmail.com>
2021-09-26 21:51:19 -0700

    implemented bfs

--- src/model/bfs.ts
+++ src/model/bfs.ts
@@ -1,5 +1,34 @@
 import { Grid, Node } from '../common'
+import { getAllNeighbors, updateUnvisitedNeighbors } from './utilities'
 
 export function bfs(grid: Grid, startNode: Node, finishNode: Node): Node[] {
-    return []
+    const visitedNodesInOrder = []
+
+    const queue = []
+    startNode.isVisited = true
+    queue.push(startNode)
+    visitedNodesInOrder.push(startNode)
+
+    while (queue) {
+        const v = queue.shift()
+        // If we encounter a wall, we skip it.
+        if (v !== undefined) {
+
+            if (v === finishNode)
+                return visitedNodesInOrder
+
+            for (const neighbor of getAllNeighbors(v, grid)) {
+                if (neighbor.isWall)
+                    continue
+
+                if (!neighbor.isVisited) {
+                    neighbor.isVisited = true
+                    visitedNodesInOrder.push(neighbor)
+                    queue.push(neighbor)
+                    updateUnvisitedNeighbors(neighbor, grid)
+                }
+            }
+        }
+    }
+    return visitedNodesInOrder
 }
